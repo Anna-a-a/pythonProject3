d594a2875b11d7cff82400e25bf2769045bdf4e9
annaalikina <alikinaan8@yandex.ru>
2024-01-13 19:53:47 +0500

    finale version

--- main.py
+++ main.py
@@ -31,39 +31,39 @@ def exchange_money(money, n, into_currency, from_currency):
 
 def max_rubles(n, rates):
     money = (100, "r")
-    for i in range(0, n):
+    for i in range(0, n - 1):
         print(money)
-        if i + 1 < n:
-            if money[1] == "r":
-                dollar = exchange_money((exchange_money(money[0], rates[i][0], "d", money[1]))[0], rates[i + 1][0], "r", "d")
-                rubles = money
-                euro = exchange_money((exchange_money(money[0], rates[i][1], "e", money[1]))[0], rates[i + 1][1], "r", "e")
-                money = max([dollar, rubles, euro], key=lambda x:x[0])
 
-            if money[1] == "d":
-                dollar = (exchange_money(money[0], rates[i + 1][0], "r", money[1]))[0]
-                rubles = (exchange_money(money[0], rates[i][0], "r", money[1]))[0]
-                r_e = exchange_money((exchange_money(money[0], rates[i][0], "r", money[1]))[0], rates[i][1], "e", "r")
-                euro = exchange_money((r_e)[0], rates[i + 1][1], "r", "e")
-                money = max([dollar, rubles, euro], key=lambda x: x[0])
+        if money[1] == "r":
+            dollar = exchange_money((exchange_money(money[0], rates[i][0], "d", money[1]))[0], rates[i + 1][0], "r", "d")
+            rubles = money
+            euro = exchange_money((exchange_money(money[0], rates[i][1], "e", money[1]))[0], rates[i + 1][1], "r", "e")
+            money = max([dollar, rubles, euro], key=lambda x:x[0])
 
-            if money[1] == "e":
-                r_d = exchange_money((exchange_money(money[0], rates[i][0], "r", money[1]))[0], rates[i][1], "d", "r")
-                dollar = exchange_money((r_d)[0], rates[i + 1][1], "r", "d")
-                rubles = (exchange_money(money[0], rates[i][0], "r", money[1]))[0]
-                euro = (exchange_money(money[0], rates[i + 1][0], "r", money[1]))[0]
-                money = max([dollar, rubles, euro], key=lambda x: x[0])
+        if money[1] == "d":
+            dollar = (exchange_money(money[0], rates[i + 1][0], "r", money[1]))[0]
+            rubles = (exchange_money(money[0], rates[i][0], "r", money[1]))[0]
+            r_e = exchange_money((exchange_money(money[0], rates[i][0], "r", money[1]))[0], rates[i][1], "e", "r")
+            euro = exchange_money((r_e)[0], rates[i + 1][1], "r", "e")
+            money = max([dollar, rubles, euro], key=lambda x: x[0])
 
-        if i == n - 1:
-            if money[1] == "d":
-                money = exchange_money(money[0], rates[i][0], "r", money[1])
-                continue
-            if money[1] == "e":
-                money = exchange_money(money[0], rates[i][1], "r", money[1])
+        if money[1] == "e":
+            r_d = exchange_money((exchange_money(money[0], rates[i][0], "r", money[1]))[0], rates[i][1], "d", "r")
+            dollar = exchange_money((r_d)[0], rates[i + 1][1], "r", "d")
+            rubles = (exchange_money(money[0], rates[i][0], "r", money[1]))[0]
+            euro = (exchange_money(money[0], rates[i + 1][0], "r", money[1]))[0]
+            money = max([dollar, rubles, euro], key=lambda x: x[0])
+
+
+    if money[1] == "d":
+        money = exchange_money(money[0], rates[i][0], "r", money[1])
+    elif money[1] == "e":
+        money = exchange_money(money[0], rates[i][1], "r", money[1])
     return money
 
 with open("output.txt", "w") as f:
-   f.write(f"{(max_rubles(int(numbers[0]), rates))[0]}")
+    result = (max_rubles(int(numbers[0]), rates))[0]
+    f.write("{:.2f}".format(round(result, 2)))
 
 # Now, 'numbers' is a list of all numbers in the file
 print(max_rubles(int(numbers[0]), rates))
